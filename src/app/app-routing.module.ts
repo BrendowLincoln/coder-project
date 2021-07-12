5a813aaf67ab6e98d0362140ac7f00e10fde6584
BrendowLincoln <linconlbrendow@gmail.com>
2021-07-12 08:58:30 -0300

    Adicionando as modificacoes de rotas e novas paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductCreateComponent } from './components/produtc/product-create/product-create.component';
 
-const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component';
+import { ProductsCrudComponent } from './views/products-crud/products-crud.component';
+
+const routes: Routes = [{
+  path: "",
+  component: HomeComponent,
+}, {
+  path: "products",
+  component: ProductsCrudComponent,
+}, {
+  path: "products/create",
+  component: ProductCreateComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-})
+},)
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'coder-project';
+  title = 'Coder Angular Project';
+
 }
